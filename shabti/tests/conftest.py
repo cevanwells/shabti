dd7f9e804e171261579ae03b5f6d9e41a096c66f
Chris Wells <chris@cevanwells.com>
2020-10-08 13:49:08 -0400

    Missed a few files on the last commit

---
+++ shabti/tests/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+import os
+
+CLIENT_ID = os.environ.get('SHABTI_CLIENT_ID', None)
+CLIENT_SECRET = os.environ.get('SHABTI_CLIENT_SECRET', None)
+ENDPOINT = os.environ.get('SHABTI_SIERRA_URL', None)
+
+
+@pytest.fixture(scope="package")
+def vcr_config():
+    return {
+        "filter_headers": [('authorization', 'DUMMY')],
+        "decode_compressed_response": True,
+        "record_mode": 'new_episodes',
+        "cassette_library_dir": 'shabti/tests/cassettes'
+    }
+
+
+@pytest.fixture(scope="package")
+def shabti_config():
+    return {
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'endpoint': ENDPOINT
+    }
