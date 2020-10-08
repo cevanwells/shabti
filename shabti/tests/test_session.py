4a1556d2cd47ed8183ca1e690bdf9d9886ba934b
Chris Wells <chris@cevanwells.com>
2020-10-08 13:57:52 -0400

    Made style changes to conform to pep8

--- shabti/tests/test_session.py
+++ shabti/tests/test_session.py
@@ -6,8 +6,12 @@ from shabti import Session
 
 @pytest.mark.vcr()
 def test_connection_token(shabti_config):
-    api = Session(shabti_config['client_id'], shabti_config['client_secret'], shabti_config['endpoint'])
+    api = Session(shabti_config['client_id'],
+                  shabti_config['client_secret'],
+                  shabti_config['endpoint'])
+
+    token_keys = ['access_token', 'token_type', 'expires_in']
 
     assert isinstance(api.token, dict)
-    assert set(['access_token', 'token_type', 'expires_in']).issubset(api.token)
+    assert set(token_keys).issubset(api.token)
     assert api.token['token_type'] == 'bearer'
