c6257edd722e2a2b6d089dd03ccf251447924ad6
Chris Wells <chris@cevanwells.com>
2020-11-03 16:25:07 -0500

    Added info() method to ResultList class

--- shabti/result_set.py
+++ shabti/result_set.py
@@ -2,10 +2,13 @@
 
 import json
 
+from shabti import Session
 from shabti import Patron
 
 
 class ResultSet(object):
+    _session = Session()
+
     def __init__(self, result_type, result_string):
         res = json.loads(result_string)
 
@@ -13,7 +16,7 @@ class ResultSet(object):
         self._start = res['start']
         self._total = res['total']
         self._entries = []
-        if self._type == 'patron':
+        if self._type == 'patrons':
             for entry in res['entries']:
                 self._entries.append(Patron(entry['link']))
 
@@ -26,6 +29,9 @@ class ResultSet(object):
     def __getitem__(self, entry_num):
         return self._entries[entry_num]
 
+    def _list(self):
+        return [str(resource.id) for resource in self._entries]
+
     @property
     def total(self):
         return self._total
@@ -37,3 +43,13 @@ class ResultSet(object):
     @property
     def entries(self):
         return self._entries
+
+    def info(self, **kwargs):
+        params = {'id': ",".join(self._list())}
+        if 'fields' in kwargs:
+            params['fields'] = kwargs['fields']
+
+        path = self._entries[0]._create_path('')
+
+        ret = self._session._request("GET", path, params=params)
+        return ret['entries']
--- shabti/tests/test_result_set.py
+++ shabti/tests/test_result_set.py
@@ -7,7 +7,7 @@ from shabti import ResourceObject
 
 
 def test_new_result_set(query_results_string):
-    res = ResultSet('patron', query_results_string)
+    res = ResultSet('patrons', query_results_string)
 
     assert isinstance(res, ResultSet)
     assert res.total == 3
@@ -17,3 +17,26 @@ def test_new_result_set(query_results_string):
     assert len(res) == 3
     assert isinstance(res, Iterable)
     assert isinstance(res[0], ResourceObject)
+
+
+@pytest.mark.vcr()
+def test_result_set_info(query_results_string, patron_keys):
+    res = ResultSet('patrons', query_results_string)
+
+    assert res._list() == ['1401559', '1401560', '1401561']
+
+    info_results = res.info()
+
+    assert set(patron_keys).issubset(info_results[0])
+    assert info_results[0]['id'] == 1401559
+    assert info_results[0]['birthDate'] == "1963-07-02"
+
+
+@pytest.mark.vcr()
+def test_result_set_info_with_fields(query_results_string, custom_patron_keys):
+    res = ResultSet('patrons', query_results_string)
+
+    info_results = res.info(fields=",".join(custom_patron_keys))
+
+    assert set(custom_patron_keys).issubset(info_results[0])
+    assert info_results[0]['names'] == ["Worthington III, Warren Kenneth"]
